7e402e4dc277a45dbb20333e5b941be7a0b3c446
casey0808 <casey_c0808@outlook.com>
2020-06-23 23:16:35 +0800

    Updates

---
+++ precache-manifest.909e93fede755a0b2b2534a68e2a8b4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "151e5a6d0a0628522d97352a6111cd34",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "e8267638ed85e9c4944f",
+    "url": "/weather-app/static/css/main.2f59e877.chunk.css"
+  },
+  {
+    "revision": "c01121369c7aa18e7b4c",
+    "url": "/weather-app/static/js/2.83784522.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather-app/static/js/2.83784522.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8267638ed85e9c4944f",
+    "url": "/weather-app/static/js/main.6ea1f75a.chunk.js"
+  },
+  {
+    "revision": "f95d929b2dc853b2601a",
+    "url": "/weather-app/static/js/runtime-main.0c118c13.js"
+  },
+  {
+    "revision": "588d2ce5f23fb14d3feb98fa85f1c36e",
+    "url": "/weather-app/static/media/billy-huynh-v9bnfMCyKbg-unsplash.588d2ce5.jpg"
+  }
+]);
